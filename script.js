5ec77c5d3362a1408d3e6f7d17dd10702a91c6bc
JOSE ACEVEDO <118148316+JAlonzo14@users.noreply.github.com>
2023-05-29 00:19:56 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,58 @@
+const textArea=document.querySelector(".text-area");
+const mensaje=document.querySelector(".mensaje");
+
+function btnEncriptar(){
+    const textoEncriptado=encriptar(textArea.value)
+    mensaje.value=textoEncriptado
+    textArea.value="";
+    mensaje.style.backgroundImage = "none"
+}
+
+
+function encriptar (stringEncriptada){
+    let matrizCodigo =[["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]];
+    stringEncriptada=stringEncriptada.toLowerCase()
+
+    for(let i=0;i<matrizCodigo.length;i++){
+        if(stringEncriptada.includes(matrizCodigo[i][0])){
+            stringEncriptada=stringEncriptada.replaceAll(matrizCodigo[i][0],matrizCodigo[i][1])
+
+
+        }
+
+    }
+    return stringEncriptada
+}
+
+function btnDesencriptar(){
+    const textoEncriptado=desencriptar(textArea.value)
+    mensaje.value=textoEncriptado
+    textArea.value="";
+    mensaje.style.backgroundImage = "none"
+}
+
+
+function desencriptar (stringDesencriptado){
+    let matrizCodigo =[["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]];
+    stringDesencriptado=stringDesencriptado.toLowerCase()
+
+    for(let i=0;i<matrizCodigo.length;i++){
+        if(stringDesencriptado.includes(matrizCodigo[i][1])){
+            stringDesencriptado=stringDesencriptado.replaceAll(matrizCodigo[i][1],matrizCodigo[i][0])
+
+
+        }
+
+    }
+    return stringDesencriptado
+}
+
+function btnCopiar() {
+    var selectedText = window.getSelection().toString();
+    if (selectedText !== "") {
+      navigator.clipboard.writeText(selectedText);
+      alert("¡Texto seleccionado copiado!");
+    } else {
+      alert("No se ha seleccionado ningún texto.");
+    }
+  }
